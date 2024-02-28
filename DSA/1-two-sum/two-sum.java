4ffeb98acd4151c3909a49a38eb0e5a18c7a8a35
Shreyas Waghchaure <72643976+Shreyas-Waghchaure@users.noreply.github.com>
2024-02-28 17:33:09 +0530

    Time: 49 ms (17.64%) | Memory: 44.9 MB (48.77%) - LeetSync

--- DSA/1-two-sum/two-sum.java
+++ DSA/1-two-sum/two-sum.java
@@ -1,11 +1,12 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] sol=new int[2];
-        for(int i=0;i<nums.length;i++){
-            for(int j= i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    sol[0]=i;
-                    sol[1] =j;
+    public int[] twoSum(int[] arr, int target) {
+        int[] sol = new int[2];
+        
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]+arr[i] == target){
+                    sol[0] = i;
+                    sol[1] = j;
                 }
             }
         }
