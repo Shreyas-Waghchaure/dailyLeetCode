303ad9ee885cf0dcbcaf031f685bf706c080b4b8
Shreyas Waghchaure <72643976+Shreyas-Waghchaure@users.noreply.github.com>
2024-02-29 12:27:28 +0530

    Time: 13 ms (21.39%) | Memory: 44.1 MB (99.30%) - LeetSync

---
+++ DSA/136-single-number/single-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+      Map<Integer,Integer> sol = new HashMap<>();
+    	int cnt =1;
+    	for(int i=0;i<nums.length;i++) {
+    		if(sol.containsKey(nums[i])) {
+    			sol.put(nums[i], cnt+1);
+    		}else {
+    			sol.put(nums[i],cnt);
+    		}
+    	}
+    	for(int num : sol.keySet()) {
+    		if(sol.get(num) == 1) {
+    			return num;
+    		}
+    	}
+        return -999;
+    }
+}
