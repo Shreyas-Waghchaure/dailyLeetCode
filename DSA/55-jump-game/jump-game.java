b854d6f4ec7ab268156665ab807805c6c6e41b83
Shreyas Waghchaure <72643976+Shreyas-Waghchaure@users.noreply.github.com>
2024-03-12 20:50:13 +0530

    Time: 3 ms (57.62%) | Memory: 45.8 MB (23.61%) - LeetSync

--- DSA/55-jump-game/jump-game.java
+++ DSA/55-jump-game/jump-game.java
@@ -1,15 +1,17 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int reach=0;
+        
+        int reach =0;
+        
         for(int i=0;i<nums.length;i++){
-           
-            if(reach<i) return false;
-            
-            reach=Math.max(reach,nums[i]+i);
-            
+            if(reach<i){
+                return false;
             }
             
+            reach = Math.max(reach,nums[i]+i);
+        }
         
-      return true;  
+        return true;
+        
     }
 }
