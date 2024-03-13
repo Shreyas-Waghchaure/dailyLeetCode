9643366fcc38f7db708802cb1be8804db20e1028
Shreyas Waghchaure <72643976+Shreyas-Waghchaure@users.noreply.github.com>
2024-03-13 09:47:07 +0530

    Time: 0 ms (100.00%) | Memory: 57.3 MB (47.13%) - LeetSync

--- DSA/189-rotate-array/rotate-array.java
+++ DSA/189-rotate-array/rotate-array.java
@@ -1,20 +1,19 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         k = k%nums.length;
-        
-        swapping(0,nums.length-1,nums);
-        swapping(0,k-1,nums);
-        swapping(k,nums.length-1,nums);
-        
+        swapper(0,nums.length-1,nums);
+        swapper(0,k-1,nums);
+        swapper(k,nums.length-1,nums);
     }
-    public static void swapping(int l,int r,int[] arr){
-        while(l<r){
-        int temp = arr[r];
-        arr[r] = arr[l];
-        arr[l] = temp;
-        l++;
-        r--;
-        
+    
+    public void swapper(int left,int right,int[] arr){
+        while(left<right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
         }
     }
+   
 }
